6a051444c455afa8a53627226ec3d7a85dd3163e
fauzul yahya <115639539+fauzul-yahya@users.noreply.github.com>
2022-11-19 17:17:01 +0700

    Update coba.js

--- coba.js
+++ coba.js
@@ -1,10 +1,21 @@
-function validasi() {
-    var nama = document.getElementById("nama").value;
-    var email = document.getElementById("email").value;
-    var alamat = document.getElementById("alamat").value;
-    if (nama =="" || email == "" ||alamat =="") {
-        alert('data tidak boleh kosong')
-    }else{
-        alert('sukses')
-    }
-        }
+function daftar(){
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var alamat = document.getElementById('alamat').value;
+  
+    if ( name == '') {
+          alert('silakan isi dengan lengkap')
+      }else if( email == ''){
+          alert('silakan isi dengan lengkap')
+      }else if (alamat == '') {
+          alert('silakan isi dengan lengkap')
+      }
+       
+       else if (name.length > 20){
+          alert('nama lengkap tidak boleh lebih 20 kata')
+      }else if( alamat.length > 100){
+          alert('alamat tidak boleh lebih dari 100 kata')
+      }else{
+          alert('sukses')
+      }
+   }
