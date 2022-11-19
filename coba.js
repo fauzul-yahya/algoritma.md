91b61dca18467a2a77aff6a685f89eb1950ce30c
fauzul yahya <115639539+fauzul-yahya@users.noreply.github.com>
2022-11-19 17:15:16 +0700

    Update coba.js

--- coba.js
+++ coba.js
@@ -1,9 +1,8 @@
 function validasi() {
-    var nomer = document.getElementById("nomer").value;
     var nama = document.getElementById("nama").value;
     var email = document.getElementById("email").value;
     var alamat = document.getElementById("alamat").value;
-    if (nomer =="" || nama =="" || email == "" ||alamat =="") {
+    if (nama =="" || email == "" ||alamat =="") {
         alert('data tidak boleh kosong')
     }else{
         alert('sukses')
