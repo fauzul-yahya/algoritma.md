f108bd1b224465e17df618018437fa37c5277156
fauzul yahya <115639539+fauzul-yahya@users.noreply.github.com>
2022-11-19 17:19:12 +0700

    Update coba.js

--- coba.js
+++ coba.js
@@ -1,9 +1,9 @@
-function daftar(){
-    var name = document.getElementById('name').value;
+function validasi(){
+    var nama = document.getElementById('nama').value;
     var email = document.getElementById('email').value;
     var alamat = document.getElementById('alamat').value;
   
-    if ( name == '') {
+    if ( nama == '') {
           alert('silakan isi dengan lengkap')
       }else if( email == ''){
           alert('silakan isi dengan lengkap')
@@ -11,7 +11,7 @@ function daftar(){
           alert('silakan isi dengan lengkap')
       }
        
-       else if (name.length > 20){
+       else if (nama.length > 20){
           alert('nama lengkap tidak boleh lebih 20 kata')
       }else if( alamat.length > 100){
           alert('alamat tidak boleh lebih dari 100 kata')
