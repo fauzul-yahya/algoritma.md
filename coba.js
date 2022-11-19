dae41ac46fcf74bc061024687bb9751b639e97ba
fauzul yahya <115639539+fauzul-yahya@users.noreply.github.com>
2022-11-19 12:17:18 +0700

    Update coba.js

--- coba.js
+++ coba.js
@@ -1,9 +1,11 @@
 function validasi() {
+    var nomer = document.getElementById("nomer").value;
     var nama = document.getElementById("nama").value;
     var email = document.getElementById("email").value;
     var alamat = document.getElementById("alamat").value;
-    if (nama != "Fauzu lyahya" && email!="@" ,"."  && alamat !="jl sunangunung jati") {
-        return true;
+    if (nomer =="" || nama =="" || email == "" ||alamat =="") {
+        alert('data tidak boleh kosong')
+    }else{
+        alert('sukses')
     }
-    else {
-        alert('isi kolom di bawah ini dengan benar');}
+        }
